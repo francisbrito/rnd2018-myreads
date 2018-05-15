138567755b4672b9bcadbe429c0666435e00bd96
Francis Brito <francis@instacarro.com>
2018-05-15 11:36:03 -0400

    chore: Update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,8 @@
 module.exports = {
   extends: 'airbnb',
+  parser: 'babel-eslint',
+  rules: {
+    'react/jsx-filename-extension': 0,
+    'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+  },
 };
