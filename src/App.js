44725c7cdeed9d4edd486c11995f455a35ca99d7
Francis Brito <francis@instacarro.com>
2018-05-15 19:46:57 -0400

    feat: Implement main view

--- src/App.js
+++ src/App.js
@@ -1,18 +1,73 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
+import * as BooksApi from './BooksApi';
+
+import { Title, Shelf, Book, ShelfChanger } from './components';
+
+const buildShelvesFromBooks = (books) => {
+  const shelves = [
+    { tag: 'Currently reading', value: 'currentlyReading' },
+    { tag: 'Want to read', value: 'wantToRead' },
+    { tag: 'Read', value: 'read' },
+  ];
+  const shelvesWithBooks = shelves.map(s => ({
+    ...s,
+    books: books.filter(b => b.shelf === s.value),
+  }));
+
+  return shelvesWithBooks;
+};
+
 class App extends Component {
+  state = {
+    books: [],
+  };
+
+  componentWillMount = () => {
+    BooksApi.getAll().then(books => this.setState(() => ({ books })));
+  };
+
+  onMovedShelf = ({ book, newShelf }) => {
+    const { books } = this.state;
+    const bookIdx = books.findIndex(b => b.title === book.title);
+
+    // DANGEROUS?
+    books[bookIdx] = Object.assign({ ...book }, { shelf: newShelf });
+
+    this.setState(() => ({ books }));
+  };
+
   render() {
+    const { books } = this.state;
+    const shelves = buildShelvesFromBooks(books);
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Title>MyReads</Title>
+        <div className="list-books-content">
+          {shelves.map(s => (
+            <Shelf
+              key={s.value}
+              {...s}
+              renderBook={b => (
+                <Book
+                  key={`${s.value}-${b.title}`}
+                  coverImageUrl={b.imageLinks.thumbnail}
+                  renderShelfChanger={() => (
+                    <ShelfChanger
+                      shelves={shelves}
+                      currentShelf={b.shelf}
+                      onMoveTo={newShelf => this.onMovedShelf({ book: b, newShelf })}
+                    />
+                  )}
+                  {...b}
+                />
+              )}
+            />
+          ))}
+        </div>
       </div>
     );
   }
