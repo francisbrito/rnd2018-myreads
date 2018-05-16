8019edf9ec3df18d471fa101af93a6d490b73334
Francis Brito <francis@instacarro.com>
2018-05-16 09:21:19 -0400

    refactor: Abstract main view into its own component

--- src/App.js
+++ src/App.js
@@ -1,76 +1,9 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import './App.css';
 
-import * as BooksApi from './BooksApi';
+import { MainView } from './components';
 
-import { Title, Shelf, Book, ShelfChanger } from './components';
-
-const buildShelvesFromBooks = (books) => {
-  const shelves = [
-    { tag: 'Currently reading', value: 'currentlyReading' },
-    { tag: 'Want to read', value: 'wantToRead' },
-    { tag: 'Read', value: 'read' },
-  ];
-  const shelvesWithBooks = shelves.map(s => ({
-    ...s,
-    books: books.filter(b => b.shelf === s.value),
-  }));
-
-  return shelvesWithBooks;
-};
-
-class App extends Component {
-  state = {
-    books: [],
-  };
-
-  componentWillMount = () => {
-    BooksApi.getAll().then(books => this.setState(() => ({ books })));
-  };
-
-  onMovedShelf = ({ book, newShelf }) => {
-    const { books } = this.state;
-    const bookIdx = books.findIndex(b => b.title === book.title);
-
-    // DANGEROUS?
-    books[bookIdx] = Object.assign({ ...book }, { shelf: newShelf });
-
-    this.setState(() => ({ books }));
-  };
-
-  render() {
-    const { books } = this.state;
-    const shelves = buildShelvesFromBooks(books);
-
-    return (
-      <div className="App">
-        <Title>MyReads</Title>
-        <div className="list-books-content">
-          {shelves.map(s => (
-            <Shelf
-              key={s.value}
-              {...s}
-              renderBook={b => (
-                <Book
-                  key={`${s.value}-${b.title}`}
-                  coverImageUrl={b.imageLinks.thumbnail}
-                  renderShelfChanger={() => (
-                    <ShelfChanger
-                      shelves={shelves}
-                      currentShelf={b.shelf}
-                      onMoveTo={newShelf => this.onMovedShelf({ book: b, newShelf })}
-                    />
-                  )}
-                  {...b}
-                />
-              )}
-            />
-          ))}
-        </div>
-      </div>
-    );
-  }
-}
+const App = () => <MainView />;
 
 export default App;
--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,4 @@ export { default as Title } from './Title';
 export { default as ShelfChanger } from './ShelfChanger';
 export { default as Book } from './Book';
 export { default as Shelf } from './Shelf';
+export { default as MainView } from './MainView';
